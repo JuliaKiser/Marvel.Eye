3e1012e7faa52d523066769149806484720b5b1c
J_Kiser <jdkiser228@gmail.com>
2021-03-12 10:04:26 -0500

    correcting gif styling

--- assets/javascript/results.js
+++ assets/javascript/results.js
@@ -1,73 +1,8 @@
-// document.addEventListener("DOMContentLoaded", displayMarvelInfo)
-// var apiKey = '0ef9234995d516e83ebba70728cb9bb2';
-// var pKey = 'ad29053b8d8b8603128abba1779970d734ec4cb8'
-
-// function displayMarvelInfo(event){
-//     var number = event.timestamp
-//     var req = XMLHttpRequest();
-//     var website = 'http://gateway.marvel.com/v1/public/characters?name='
-//     var name =document.getElementById('heroName').value;
-//     var web = website + name + "ts=" + number + '&apikey=' + apiKey + '&hash=' + pKey;
-
-//     document.getElementById('submit').addEventListener('click', function (event) {
-//     document.getElementById('heroName').textContent = "";
-//     document.getElementById('description').textContent = "";
-//     });
-//     req.open('GET', web, true);
-
-//     req.setRequestHeader('Content-Type', 'application/json');
-//     req.addEventListener('load',function(){
-//         if(req.status >= 200 && req.status < 400){
-//             var result = JSON.parse(req.responseText);
-//             document.getElementById('name').textContent = result.name;
-//             document.getElementById('description').textContent = result.description;
-//         }
-
-//         event.preventDefault();
-//     });
-//     req.send(null);
-// }
-    
-
-// 
-
-// trying to get a function to display a picture when option is selected
-// function setHero(){
-//     var img =document.getElementById("Wonder Woman");
-//     img.src =this.value;
-//     return false;
-// }
-// document.getElementById("characters").replace(".image-placeholder");
-
-// $(document).ready(function() {
-//     $('#characters').on('change', function() {
-//         if ([ 'Wonder Woman', 'Captain America' ].indexOf(this.value) > -1)
-//         {
-//             $("./Images/wonder-woman.jpg").show()
-//         }
-//         else 
-//         {
-//             $("./Images/wonder-woman.jpg").hide();
-//         }
-//     })
-//     .change();
-// });
-
-// https://www.youtube.com/yt/dev/api-resources.html
-// AIzaSyDOYsiDFdX1sy4JHyCuUeOf8oTDcvG4Jfk
-// https://developer.marvel.com/
-// Marvel Public Key - 0ef9234995d516e83ebba70728cb9bb2
-// developer.marvel.com
-// document.addEventListener("DOMContentLoaded", bindButtons)
-
-
 $(document).ready(function() {
     $("#marvel-form").hide();
     $(".select-features").click(function() {
       $("#marvel-form").toggle();
     });
-  
-    
 
 var apiKey = '0ef9234995d516e83ebba70728cb9bb2';
 var pKey = 'ad29053b8d8b8603128abba1779970d734ec4cb8'
@@ -88,7 +23,7 @@ function landingGiphy() {
         console.log(response)
 
         //Adds image to landing page--replaces quote
-        $("#thumbnail").append("<img src='" + response.data[0].images.original.url + "' style='padding-left: 100px; padding-top:100px;'/>")
+        $("#thumbnail").append("<img src='" + response.data[0].images.original.url + "'/>")
         //$(".side-x").html(superHeroGiphy);
 
     })
@@ -130,3 +65,4 @@ $("#marvel-form").on("submit", function(event) {
 })
 
 });
+
